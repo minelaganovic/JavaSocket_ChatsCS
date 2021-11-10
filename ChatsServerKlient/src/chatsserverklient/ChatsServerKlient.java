b32fbe83a054d4e2681823f45289e83200acdb32
Minela Ganovic <59210673+minelaganovic@users.noreply.github.com>
2021-11-10 18:51:06 +0100

    Add files via upload

---
+++ ChatsServerKlient/src/chatsserverklient/ChatsServerKlient.java
@@ -0,0 +1,12 @@
+package chatsserverklient;
+
+public class ChatsServerKlient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
